61ed1112b69028ee22e962a25cbcd0de756f428b
Parag Paralikar <parag.paralikar@gmail.com>
2022-02-05 21:50:44 +0530

    na

--- llms/src/main/java/com/revosystems/llms/WebSecurityConfiguration.java
+++ llms/src/main/java/com/revosystems/llms/WebSecurityConfiguration.java
@@ -1,46 +1,59 @@
 package com.revosystems.llms;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers.provisioning.JdbcUserDetailsManagerConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	private DataSource dataSource;
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http
-			.authorizeRequests()
-				.antMatchers("/", "/home.html").permitAll()
+		http.authorizeRequests().antMatchers("/", "/h2", "/h2/**").permitAll()
 				.anyRequest().authenticated()
-				.and()
-			.formLogin()
-				//.loginPage("/login")
-				.permitAll()
-				.successForwardUrl("/home.html")
-				.and()
-			.logout()
-				.permitAll();
+				.and().csrf().ignoringAntMatchers("/h2/**")
+				.and().headers().frameOptions().disable()
+				.and().formLogin().permitAll().and()
+				.logout().permitAll();
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		final JdbcUserDetailsManagerConfigurer<AuthenticationManagerBuilder> configurer = auth.jdbcAuthentication()
+				.dataSource(dataSource)
+				.passwordEncoder(passwordEncoder());
+		
+		if (!dataSource.getConnection().getMetaData().getTables(null, "", "USERS", null).first()) {
+			configurer.withDefaultSchema()
+				.withUser("parag")
+				.password(passwordEncoder().encode("parag"))
+				.roles("USER");
+		}
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder(12);
 	}
 
 	@Bean
 	@Override
-	public UserDetailsService userDetailsService() {
-		UserDetails user =
-			 User.withDefaultPasswordEncoder()
-				.username("user")
-				.password("password")
-				.roles("USER")
-				.build();
-
-		return new InMemoryUserDetailsManager(user);
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
 	}
-	
+
 }
