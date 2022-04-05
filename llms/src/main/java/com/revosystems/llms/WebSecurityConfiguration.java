f9dd5efc275e2860aa4cf0a6bac4a88e0c955ebf
chetan30011996 <chtnpatel.46.cp@gmail.com>
2022-04-05 12:06:23 +0530

    UI

--- llms/src/main/java/com/revosystems/llms/WebSecurityConfiguration.java
+++ llms/src/main/java/com/revosystems/llms/WebSecurityConfiguration.java
@@ -33,13 +33,13 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter imple
 	
 	@Override
     public void configure(WebSecurity web) throws Exception {
-      web.ignoring().antMatchers("/js/**", "/images/**", "/css/**"); // #3
+		web.ignoring().antMatchers("/static/js/*", "/images/**", "/static/css/**", "/manifest.json"); // #3
     }
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-			.antMatchers("/", "/h2", "/h2/**", "/favicon.ico", "/js/**", "/images/**", "/css/**").permitAll()
+				.antMatchers("/", "/h2", "/h2/**", "/favicon.ico","/static/js/**", "/images/**", "/static/css/**").permitAll()
 			.anyRequest().authenticated().and()
 			.csrf().disable()
 			.cors().disable()
